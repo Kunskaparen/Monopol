53e7a139468bd80361370e9c9e7bec8aecc28cac
Kunskaparen <henrik.akerberg@hotmail.com>
2015-01-07 23:53:11 +0100

    HEJ

--- Script.js
+++ Script.js
@@ -1,6 +1,8 @@
 //Ogge är ful i mun
-
-var spelPlan = document.getElementById("spelPlan");
-var context = spelPlan.getContext("2d");
-var planBild = "Monopol.png";
-context.drawImage(planBild, 120, 100)
+document.addEventListener("DOMContentLoaded", function(event) { 
+		var spelPlan = document.getElementById("spelPlan");
+		var context = spelPlan.getContext("2d");
+		var planBild = "Monopol.png";
+		context.drawImage(planBild, 120, 100)
+		alert("HEJ")
+});
