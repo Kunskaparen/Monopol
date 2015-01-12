4387b63c4031cbe3946705302973e6f48c4b109a
Kunskaparen <henrik.akerberg@hotmail.com>
2015-01-12 23:23:48 +0100

    NU jvlar

--- Script.js
+++ Script.js
@@ -1,8 +1,18 @@
-//Ogge är ful i mun
+﻿//Ogge är ful i mun
 document.addEventListener("DOMContentLoaded", function(event) { 
+		//Det här autostartas
 		var spelPlan = document.getElementById("spelPlan");
 		var context = spelPlan.getContext("2d");
 		var planBild = "Monopol.png";
 		context.drawImage(planBild, 120, 100)
-		alert("HEJ")
 });
+
+function die() {
+	antalRullningar = 20 //Hur många gånger tärningen rullar innan den stannar
+	results = [] //Alla rullningar tärningen gör
+	for (i = 0; i<antalRullningar;i++){
+	    //genererar alla resultat, sista i results är det som man flyttar efter
+		results.push(Math.floor((Math.random() * 6) +1);
+	}
+	for
+}
