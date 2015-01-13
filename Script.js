289d64aada77a92ce417bc4de2e868a914969ff5
Kunskaparen <henrik.akerberg@hotmail.com>
2015-01-13 11:42:25 +0100

    Tärningen e inte klar

--- Script.js
+++ Script.js
@@ -1,11 +1,15 @@
 ﻿//Ogge är ful i mun
-document.addEventListener("DOMContentLoaded", function(event) { 
-		//Det här autostartas
-		var spelPlan = document.getElementById("spelPlan");
-		var context = spelPlan.getContext("2d");
-		var planBild = "Monopol.png";
-		context.drawImage(planBild, 120, 100)
-});
+window.onload = function init() {
+	var spelPlan = document.getElementById("spelPlan");
+	var context = spelPlan.getContext("2d");
+	var planBild = new Image();
+	planBild.src = "Monopol.svg";
+	planBild.onload = function() {
+	context.drawImage(planBild,  0, 0, 250, 250);
+	alert("HEJ");
+	}
+alert("HEWW");
+}
 
 function die() {
 	antalRullningar = 20 //Hur många gånger tärningen rullar innan den stannar
