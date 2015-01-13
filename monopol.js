a3dd7f078b3642f672c57faa4e2b6019c8c9a29c
Kunskaparen <henrik.akerberg@hotmail.com>
2015-01-13 19:01:13 +0100

    Nu är tärningen fixad, och planbilden är mer generell

--- monopol.js
+++ monopol.js
@@ -1,16 +1,19 @@
 //Ogge är ful i mun 
 window.onload = function init() {
-	var canvas = document.createElement("canvas");
+	canvas = document.createElement("canvas");
 	canvas.setAttribute("id", "spelPlan");
-	canvas.height = 400; //Ändra planens storlek här
-	canvas.width = 400;  //Och här
-	
+	//canvas.height = 400; 
+	//canvas.width = 400;  
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
 	document.getElementById("farsa").appendChild(canvas);
-	var context = document.getElementById("spelPlan").getContext("2d");
+	context = document.getElementById("spelPlan").getContext("2d");
 	var planBild = new Image();
 	planBild.src = "Monopol.png";
-	planBild.onload = function() { 
-	context.drawImage(planBild, canvas.width / 2 - 200, 0, canvas.height, canvas.height);
+	planBild.width = 600; //Ändra planens storlek här
+	planBild.height = 600; // Och här
+	planBild.onload = function() {
+		context.drawImage(planBild, canvas.width / 2 - planBild.width/2, 0, planBild.width, planBild.height);
 	
 	};
 };
@@ -35,30 +38,33 @@ function die() {
 	var fyra = new Image();
 	var femma = new Image();
 	var sexa = new Image();
-	etta.src = "etta.png"
-	tvåa.src = "tvåa.png"
-	trea.src = "trea.png"
-	fyra.src = "fyra.png"
-	femma.src = "femma.png"
-	sexa.src = "sexa.png"
-	for (item in results){
+	etta.src = "etta.png";
+	tvåa.src = "tvåa.png";
+	trea.src = "trea.png";
+	fyra.src = "fyra.png";
+	femma.src = "femma.png";
+	sexa.src = "sexa.png";
+	var tärningssidor = {1:etta, 2:tvåa, 3:trea, 4:fyra, 5:femma, 6:sexa};
+	for (var item in results){
+		item = results[item]
+		console.log(item)
 		if (item === 1){
-			context.drawImage("etta.png");
+			context.drawImage("etta.png",canvas.width / 2 + planBild.width/2, 0);
 		}
 		else if (item === 2){
-			context.drawImage("tvåa.png");
+			context.drawImage("tvåa.png",canvas.width / 2 + planBild.width/2, 0);
 		}
 		else if (item === 3){
-			context.drawImage("trea.png");
+			context.drawImage("trea.png",canvas.width / 2 + planBild.width/2, 0);
 		}
 		else if (item === 4){
-			context.drawImage("fyra.png");
+			context.drawImage("fyra.png",canvas.width / 2 + planBild.width/2, 0);
 		}
 		else if (item === 5){
-			context.drawImage("femma.png");
+			context.drawImage("femma.png",canvas.width / 2 + planBild.width/2, 0);
 		}
 		else if (item === 6){
-			context.drawImage("sexa.png");
+			context.drawImage("sexa.png",canvas.width / 2 + planBild.width/2, 0);
 		}
 		
 	}
@@ -70,4 +76,3 @@ function ritaPjäs(ruta) {
 				16:"897 897", 17:"788 898", 18:"680 898"}
 	
 	}
-}
