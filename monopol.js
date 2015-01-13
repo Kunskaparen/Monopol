d13d9288a90074f493ecd0ca08bb52346c7e7f97
Ogerator <oskar.soderberg@live.com>
2015-01-13 14:27:47 +0100

    Fixa planens storlek

--- monopol.js
+++ monopol.js
@@ -1,11 +1,16 @@
 //Ogge är ful i mun
 window.onload = function init() {
+	var canvas = document.createElement("canvas");
+	canvas.setAttribute("id", "spelPlan")
+	canvas.height = 300; //Ändra planens storlek här
+	canvas.width = 300;  //Och här
+	document.getElementById("farsa").appendChild(canvas)
 	var spelPlan = document.getElementById("spelPlan");
 	var context = spelPlan.getContext("2d");
 	var planBild = new Image();
-	planBild.src = "Monopol.svg";
+	planBild.src = "Monopol.png";
 	planBild.onload = function() {
-	context.drawImage(planBild,  0, 0, 250, 250);
+	context.drawImage(planBild,  0, 0, canvas.height, canvas.height);
 	};
 };
 
@@ -24,5 +29,6 @@ function die() {
 	}
 	console.log(results);
 	for (item in results){
-	
+		console.log("HEJ")
+	}
 }
